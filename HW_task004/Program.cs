c3fa6ddbc811868f0f2b151eb0723ba54e37ed61
Alexandr25041973 <sashan25041973@yandex.ru>
2023-06-24 21:17:37 +0300

    Add Home Work 002-004-006-006

---
+++ HW_task004/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+Домашнее задание. 
+Задача №4: Напишите программу, которая на вход 
+принимает три числа и выдает максимальное из этих чисел.
+Пример:
+2, 3, 7 -> 7
+44 5 78 -> 78 
+22 3 9 -> 22
+*/
+Console.Write("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int numberC = int.Parse(Console.ReadLine());
+int max = numberA;
+if(max > numberB)
+{
+  if(max > numberC)
+  {
+    Console.Write(max);
+  }
+  else
+  {
+    max = numberC;
+  }
+}
+else
+{
+  max = numberB;
+  if(max > numberC)
+  {
+    Console.Write(max);
+  }
+  else
+  {
+    max = numberC;
+  }
+}
+Console.Write(max);
---
+++ HW_task008/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+Console.Clear();
+/*
+Домашнее задание. 
+Задача №8: Напишите программу, которая на вход принимает число (N), а на выходе 
+показывает все четные числа от 1 до N.
+Пример:
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+Console.Write("Введите целое число от 1 до N: ");
+int numberN = int.Parse(Console.ReadLine());
+int count = 2;
+if(numberN == 1)
+{
+  Console.Write("Нет четных чисел от 1 до N");
+}
+else
+{
+  if(numberN%2 == 0)
+  {
+    while(count < numberN + 1)
+    {
+      Console.Write(count);
+      if(count == numberN)
+      {
+        Console.Write(" ");
+      }
+      else
+      {
+        Console.Write(", ");
+      }
+      count+=2;
+    }    
+  }
+  else
+  {
+    while(count < numberN)
+    {
+      Console.Write(count);
+      if(count == numberN - 1)
+      {
+        Console.Write(" ");
+      }
+      else
+      {
+        Console.Write(", ");
+      }
+      count+=2;
+    }    
+  }
+}
